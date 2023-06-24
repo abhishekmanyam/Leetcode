ba611af1a32291dda665087a08c383a3efd1990f
abhishekmanyam <abhishekmanyam@gmail.com>
2023-06-23 17:01:08 -0700

    day2

---
+++ CConDS/Sum_of_Squares.py
@@ -0,0 +1,6 @@
+nums = [-4,-1,0,3,10]
+
+for i in range(len(nums)):
+    nums[i] = nums[i]*nums[i]
+nums = sorted(nums)
+print(nums)
--- CConDS/reverse_a_string.py
+++ CConDS/reverse_a_string.py
@@ -1,3 +1,13 @@
-print("hello world")
-#good to know that i can work on git
-#cool methods
+
+#let us say that the inout array is given as vector s
+
+s = ['a','b','v','c','x','d']
+lengthofarray = len(s)
+i =0
+while i < lengthofarray:
+    s[lengthofarray-1],s[i] = s[i],s[lengthofarray-1]
+    i+=1
+    lengthofarray-=1
+
+print(s)
+
